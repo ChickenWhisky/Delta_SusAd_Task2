a87ce782df91ca11277b07ea41a56f032d52f0eb
thomas <thomasmampilli2003@gmail.com>
2023-06-14 16:25:20 +0530

    made changes to gamma-z.hm.conf & delted index.html

--- initialise.sh
+++ initialise.sh
@@ -3,9 +3,17 @@
 sudo apt install apache2 -y
 sudo a2enmod proxy
 sudo a2enmod proxy_http
+sudo ufw app list
+sudo ufw allow in "Apache"
+sudo mkdir /var/www/gamma-z.hm
 
-cp ./gamma-z.hm.conf /etc/apache2/sites-available/
+sudo cp ./index.html /var/www/gamma-z.hm/index.html
+
+sudo cp ./gamma-z.hm.conf /etc/apache2/sites-available/
 sudo ln -s /etc/apache2/sites-available/gamma-z.hm.conf /etc/apache2/sites-enabled/gamma-z.hm.conf
+sudo a2ensite gamma-z.hm.conf
+sudo a2dissite 000-default.conf
+
 
 echo "127.0.0.1 gamma-z.hm" | sudo tee -a /etc/hosts > /dev/null
-sudo systemctl restart apache2
+sudo systemctl reload apache2
