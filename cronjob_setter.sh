f5f00920e6b3491e5b33e0a24411a5c8900ba931
thomas <thomasmampilli2003@gmail.com>
2023-06-20 00:35:46 +0530

    Changed the conf file

--- cronjob_setter.sh
+++ cronjob_setter.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 
-cron_expression="10 10 3,6,9,12,15,18,21,24,27,30 5,6,8 * [ \"\$(date '+\%a')\" = \"Sun\" ] && ./backup.sh"
+cron_expression1="0 0 */3 5,6,8 * [ \"\$(date '+\%a')\" = \"Sun\" ] && ./backup.sh"
+
+#Makes sure that it runs on sundays and at the same time it isnt the date is a multiple of 3
+cron_expression2="0 0 * 1,2,6 * [ "$(date '+\%a' -d tomorrow)" == "Sun" ] && [ "$(date '+\%m')" != "03" ] && ./backup.sh"
+
 
 # Add the cron job
-(crontab -l 2>/dev/null; echo "$cron_expression") | crontab -
+(crontab -l 2>/dev/null; echo "$cron_expression1") | crontab -
+(crontab -l 2>/dev/null; echo "$cron_expression2") | crontab -
 
 echo "Cron job for database backup has been set up."
---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Done so that we can make servermess.sh run in the background and allow us to 
+# free up and utilize the terminal for other purposes
+
+nohup /home/bigBrother/cronjob_setter.sh &
+exec /bin/bash
